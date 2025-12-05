c35dff0eee085f9fd8a0649a6eaf29704e013749
sam-k-varghese <ksam071@gmail.com>
2025-12-05 10:52:54 +0530

    create:[Sam]-solved basic linkedlist prob

---
+++ Python Practice/linked-list/remove_nth_last.py
@@ -0,0 +1,30 @@
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        for _ in range(n):
+            fast = fast.next
+
+        if fast == None:
+            return head.next
+
+        while fast.next is not None:
+            slow=slow.next
+            fast=fast.next
+        
+        slow.next = slow.next.next
+        
+        return head
+        
